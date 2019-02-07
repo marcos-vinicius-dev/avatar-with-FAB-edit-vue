5e147ffb3a43b8e010d43986a999c43a3876df90
Marcos Vinicius <37404158+marcos-vinicius-dev@users.noreply.github.com>
2019-02-06 23:20:53 -0200

    Create avatar.vue

---
+++ avatar.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-card color="white">
+        <v-card-text>
+            <div class="text-xs-center">
+            <v-avatar
+              size="90px"
+            >
+            <img
+                src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+                alt="Avatar"
+            >
+            <v-btn
+            class="avatar-edit"
+              dark
+              right
+              small
+              fab
+              color="blue_2"
+            >
+                <v-icon>edit</v-icon>
+            </v-btn>
+            </v-avatar>
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+<style>
+    .avatar-edit{
+        margin-top: 60px;
+        margin-left: -10px;
+    }
+</style>
+
